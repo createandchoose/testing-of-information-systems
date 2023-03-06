5770d32f82d4ab4b493700638bd84c50dcea0cbc
Aytal Popov <aytal.popov2002@yandex.ru>
2023-03-06 13:08:41 +0900

    asd
    asd

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+import { useSanityClient } from 'astro-sanity';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import sanity from "astro-sanity";
+import cloudflare from "@astrojs/cloudflare";
+
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://thefoxxstuff-net.pages.dev',
+    integrations: [mdx(), sitemap(), sanity({
+        projectId: '2xd7irb9',
+        dataset: 'production',
+        apiVersion: '2021-03-25',
+        useCdn: true
+    }), alpinejs()],
+    output: 'server' // adapter: cloudflare()
+        ,
+    adapter: cloudflare()
+});
---
+++ src/data/settings.ts
@@ -0,0 +1,26 @@
+export const settings = {
+  site: 'https://',
+  name: '',
+  title: '',
+  description: '',
+  showLittleSticksPlug: true,
+}
+
+
+// tisinfo@ro.ru
+// dGmQMD!1
+
+// Контрольный вопрос:
+// пушкин
+
+
+
+// sanity
+// tisinfo@ro.ru
+// dH7-Bc6-Sxj-2HR
+
+
+// storj
+// tisinfo@ro.ru
+// tisinfo@ro.ru
+// maple solve monster search ritual discover ahead disorder bubble half marble culture
